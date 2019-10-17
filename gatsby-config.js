fc044620cd850d98bf189582a3ef6aa3117e92e1
Ursulet Aurélien <aurelien.ursulet@gmail.com>
2019-10-17 16:30:04 +0200

    ouech

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/product`,
+        name: `product`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [

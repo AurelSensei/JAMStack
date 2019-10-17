b38c3379e48f5a657dfd811e66632c5c95c3aac5
Ursulet Aurélien <aurelien.ursulet@gmail.com>
2019-10-17 16:04:23 +0200

    ouech

--- src/templates/product-post.js
+++ src/templates/product-post.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
 
-class BlogPostTemplate extends React.Component {
+class ProductPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
@@ -80,10 +80,10 @@ class BlogPostTemplate extends React.Component {
   }
 }
 
-export default BlogPostTemplate
+export default ProductPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
+  query ProductPostBySlug($slug: String!) {
     site {
       siteMetadata {
         title
